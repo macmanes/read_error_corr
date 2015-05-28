ee06e3035b178be7270e450122ca8e17f65a8d68
Matt MacManes <matthew.macmanes@unh.edu>
2015-05-28 16:01:22 -0400

    added stats

--- correct.mk
+++ correct.mk
@@ -4,6 +4,9 @@ SHELL=/bin/bash -o pipefail
 
 #USAGE:
 #	./correct.mk all CPU=16 SAMP=10
+#
+#
+#
 
 MAKEDIR := $(dir $(firstword $(MAKEFILE_LIST)))
 DIR := ${CURDIR}
@@ -12,7 +15,7 @@ SAMP=10
 #the number of reads in millions to subsample.  
 
 
-all: scripts download_reads subsamp_reads reference raw lighter bless sga bfc seecer
+all: scripts download_reads subsamp_reads reference raw lighter bless sga bfc seecer stats
 
 
 scripts:
@@ -29,7 +32,7 @@ download_reads:
 	gzip -cd <(wget -qO- ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR797/SRR797058/SRR797058_2.fastq.gz) > SRR797058_2.fastq
 subsamp_reads:
 	cd ${DIR}/reads && \
-	python ${DIR}/scripts/subsampler.py $(SAMP)000000 SRR797058_1.fastq SRR797058_2.fastq && \
+	python ${DIR}/scripts/subsampler.py ${SAMP}000000 SRR797058_1.fastq SRR797058_2.fastq && \
 	sed -i 's_ H_-H_g' subsamp_{1,2}.fastq
 
 reference:
@@ -42,52 +45,65 @@ reference:
 raw:
 	mkdir -p ${DIR}/raw
 	cd ${DIR}/raw && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus ${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq > $(SAMP)M.raw.sam
+	bwa mem -t $(CPU) ${DIR}/genome/mus ${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq > ${SAMP}M.raw.sam
 
 lighter:
-	mkdir -p ${DIR}/lighter$(SAMP)M
-	cd ${DIR}/lighter$(SAMP)M && \
+	mkdir -p ${DIR}/lighter${SAMP}M
+	cd ${DIR}/lighter${SAMP}M && \
 	lighter -K 31 60000000 -r ${DIR}/reads/subsamp_1.fastq -r ${DIR}/reads/subsamp_2.fastq -t $(CPU) && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus subsamp_1.cor.fq subsamp_2.cor.fq > $(SAMP)M.lighter.sam
+	bwa mem -t $(CPU) ${DIR}/genome/mus subsamp_1.cor.fq subsamp_2.cor.fq > ${SAMP}M.lighter.sam
 
 
 
 bless:
-	mkdir -p ${DIR}/bless$(SAMP)M
-	mkdir -p ${DIR}/bless$(SAMP)M/kmc/bin/
-	cp /home/ubuntu/v0p24/kmc/bin/kmc /mnt/bless$(SAMP)M/kmc/bin/kmc
-	cd ${DIR}/bless$(SAMP)M && \
-	mpirun -np $(CPU) bless -read1 ${DIR}/reads/subsamp_1.fastq -read2 ${DIR}/reads/subsamp_2.fastq -prefix $(SAMP)M_bless55 -kmerlength 55 && \
-	mpirun -np $(CPU) bless -read1 ${DIR}/reads/subsamp_1.fastq -read2 ${DIR}/reads/subsamp_2.fastq -prefix $(SAMP)M_bless33 -kmerlength 33 && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus $(SAMP)M_bless55.1.corrected.fastq $(SAMP)M_bless55.2.corrected.fastq > $(SAMP)M.bless55.sam && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus $(SAMP)M_bless33.1.corrected.fastq $(SAMP)M_bless33.2.corrected.fastq > $(SAMP)M.bless33.sam
+	mkdir -p ${DIR}/bless${SAMP}M
+	mkdir -p ${DIR}/bless${SAMP}M/kmc/bin/
+	cp /home/ubuntu/v0p24/kmc/bin/kmc /mnt/bless${SAMP}M/kmc/bin/kmc
+	cd ${DIR}/bless${SAMP}M && \
+	mpirun -np $(CPU) bless -read1 ${DIR}/reads/subsamp_1.fastq -read2 ${DIR}/reads/subsamp_2.fastq -prefix ${SAMP}M_bless55 -kmerlength 55 && \
+	mpirun -np $(CPU) bless -read1 ${DIR}/reads/subsamp_1.fastq -read2 ${DIR}/reads/subsamp_2.fastq -prefix ${SAMP}M_bless33 -kmerlength 33 && \
+	bwa mem -t $(CPU) ${DIR}/genome/mus ${SAMP}M_bless55.1.corrected.fastq ${SAMP}M_bless55.2.corrected.fastq > ${SAMP}M.bless55.sam && \
+	bwa mem -t $(CPU) ${DIR}/genome/mus ${SAMP}M_bless33.1.corrected.fastq ${SAMP}M_bless33.2.corrected.fastq > ${SAMP}M.bless33.sam
 
 
 sga:
-	mkdir -p ${DIR}/sga$(SAMP)M
-	cd ${DIR}/sga$(SAMP)M && \
+	mkdir -p ${DIR}/sga${SAMP}M
+	cd ${DIR}/sga${SAMP}M && \
 	sga preprocess -p 1 ${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq | gzip -1 > out.pe.fq.gz && \
 	sga index -a ropebwt -t 8 --no-reverse out.pe.fq.gz && \
 	sga correct -t $(CPU) -k 55 --learn out.pe.fq.gz -o sga.55.fq && \
 	sga correct -t $(CPU) -k 33 --learn out.pe.fq.gz -o sga.33.fq && \
 	split-paired-reads.py sga.55.fq && \
 	split-paired-reads.py sga.33.fq && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus sga.55.fq.1 sga.55.fq.2 > $(SAMP)M.sga55.sam && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus sga.33.fq.1 sga.33.fq.2 > $(SAMP)M.sga33.sam
+	bwa mem -t $(CPU) ${DIR}/genome/mus sga.55.fq.1 sga.55.fq.2 > ${SAMP}M.sga55.sam && \
+	bwa mem -t $(CPU) ${DIR}/genome/mus sga.33.fq.1 sga.33.fq.2 > ${SAMP}M.sga33.sam
 
 
 bfc:
-	mkdir -p ${DIR}/bfc$(SAMP)M
-	cd ${DIR}/bfc$(SAMP)M && \
+	mkdir -p ${DIR}/bfc${SAMP}M
+	cd ${DIR}/bfc${SAMP}M && \
 	interleave-reads.py ${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq -o inter.fq && \
 	bfc -s 50m -k55 -t 16 inter.fq > bfc55.corr.fq && \
 	bfc -s 50m -k33 -t 16 inter.fq > bfc33.corr.fq && \
 	split-paired-reads.py bfc55.corr.fq && \
 	split-paired-reads.py bfc33.corr.fq && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus bfc55.corr.fq.1 bfc55.corr.fq.2 > $(SAMP)M.bfc55.sam && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus bfc33.corr.fq.1 bfc33.corr.fq.2 > $(SAMP)M.bfc33.sam
+	bwa mem -t $(CPU) ${DIR}/genome/mus bfc55.corr.fq.1 bfc55.corr.fq.2 > ${SAMP}M.bfc55.sam && \
+	bwa mem -t $(CPU) ${DIR}/genome/mus bfc33.corr.fq.1 bfc33.corr.fq.2 > ${SAMP}M.bfc33.sam
 
 seecer:
-	mkdir -p ${DIR}/seecer$(SAMP)M
-	cd ${DIR}/seecer$(SAMP)M && \
-	run_seecer.sh -t . -k 31 ${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq
+	mkdir -p ${DIR}/seecer${SAMP}M
+	cd ${DIR}/seecer${SAMP}M && \
+	run_seecer.sh -t . -k 31 ${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq && \
+	bwa mem -t $(CPU) ${DIR}/genome/mus ${DIR}/reads/subsamp_1.fastq_corrected.fa ${DIR}/reads/subsamp_2.fastq_corrected.fa > ${SAMP}M.seecer.sam
+
+stats:
+	mkdir -p ${DIR}/stats${SAMP}M
+	cd ${DIR}/stats${SAMP}M && \
+	k8 ~/bfc/errstat.js ${DIR}/bfc${SAMP}M/${SAMP}M.bfc55.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -17 > ${SAMP}M.bfc55.out && \
+	k8 ~/bfc/errstat.js ${DIR}/bfc${SAMP}M/${SAMP}M.bfc33.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -17 > ${SAMP}M.bfc33.out && \
+	k8 ~/bfc/errstat.js ${DIR}/sga${SAMP}M/${SAMP}M.sga55.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -17 > ${SAMP}M.sga33.out && \
+	k8 ~/bfc/errstat.js ${DIR}/sga${SAMP}M/${SAMP}M.sga33.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -17 > ${SAMP}M.sga33.out && \
+	k8 ~/bfc/errstat.js ${DIR}/bless${SAMP}M/${SAMP}M.bless33.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -17 > ${SAMP}M.bless33.out && \
+	k8 ~/bfc/errstat.js ${DIR}/bless${SAMP}M/${SAMP}M.bless33.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -17 > ${SAMP}M.bless33.out && \
+	k8 ~/bfc/errstat.js ${DIR}/lighter${SAMP}M/${SAMP}M.lighter.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -17 > ${SAMP}M.lighter.out && \
+	k8 ~/bfc/errstat.js ${DIR}/seecer${SAMP}M/${SAMP}M.seecer.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -17 > ${SAMP}M.seecer.out
