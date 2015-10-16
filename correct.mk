561464f9bde55bd41cbfc86dbb6d9a01c6fda267
Matt MacManes <matthew.macmanes@unh.edu>
2015-10-16 14:58:04 -0400

    label SAMP

--- correct.mk
+++ correct.mk
@@ -41,19 +41,19 @@ reference:
 
 subsamp_reads:
 	cd ${DIR}/reads && \
-	seqtk sample -s102340 ${READ1} ${SAMP}000000 > subsamp_1.fastq && \
-	seqtk sample -s102340 ${READ2} ${SAMP}000000 > subsamp_2.fastq && \
+	seqtk sample -s102340 ${READ1} ${SAMP}000000 > ${SAMP}.subsamp_1.fastq && \
+	seqtk sample -s102340 ${READ2} ${SAMP}000000 > ${SAMP}.subsamp_2.fastq && \
 	sed -i 's_ H_-H_g' subsamp_{1,2}.fastq
 
-raw:${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq
+raw:${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq
 	mkdir -p ${DIR}/raw
 	cd ${DIR}/raw && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus ${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq > ${SAMP}M.raw.sam
+	bwa mem -t $(CPU) ${DIR}/genome/mus ${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq > ${SAMP}M.raw.sam
 
-lighter:${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq
+lighter:${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq
 	mkdir -p ${DIR}/lighter${SAMP}M
 	cd ${DIR}/lighter${SAMP}M && \
-	lighter -K 31 60000000 -r ${DIR}/reads/subsamp_1.fastq -r ${DIR}/reads/subsamp_2.fastq -t $(CPU) && \
+	lighter -K 31 60000000 -r ${DIR}/reads/${SAMP}.subsamp_1.fastq -r ${DIR}/reads/${SAMP}.subsamp_2.fastq -t $(CPU) && \
 	bwa mem -t $(CPU) ${DIR}/genome/mus subsamp_1.cor.fq subsamp_2.cor.fq > ${SAMP}M.lighter.sam && \
 	k8 ~/bfc/errstat.js ${DIR}/lighter${SAMP}M/${SAMP}M.lighter.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.lighter.out
 
@@ -64,8 +64,8 @@ lighter_trinity:
 bless:
 	mkdir -p ${DIR}/bless${SAMP}M
 	cd ${DIR}/bless${SAMP}M && \
-	mpirun -np $(CPU) bless -notrim -read1 ${DIR}/reads/subsamp_1.fastq -read2 ${DIR}/reads/subsamp_2.fastq -prefix ${SAMP}M_bless55 -kmerlength 55 && \
-	mpirun -np $(CPU) bless -notrim -read1 ${DIR}/reads/subsamp_1.fastq -read2 ${DIR}/reads/subsamp_2.fastq -prefix ${SAMP}M_bless31 -kmerlength 31 && \
+	mpirun -np $(CPU) bless -notrim -read1 ${DIR}/reads/${SAMP}.subsamp_1.fastq -read2 ${DIR}/reads/${SAMP}.subsamp_2.fastq -prefix ${SAMP}M_bless55 -kmerlength 55 && \
+	mpirun -np $(CPU) bless -notrim -read1 ${DIR}/reads/${SAMP}.subsamp_1.fastq -read2 ${DIR}/reads/${SAMP}.subsamp_2.fastq -prefix ${SAMP}M_bless31 -kmerlength 31 && \
 	bwa mem -t $(CPU) ${DIR}/genome/mus ${SAMP}M_bless55.1.corrected.fastq ${SAMP}M_bless55.2.corrected.fastq > ${SAMP}M.bless55.sam && \
 	bwa mem -t $(CPU) ${DIR}/genome/mus ${SAMP}M_bless31.1.corrected.fastq ${SAMP}M_bless31.2.corrected.fastq > ${SAMP}M.bless31.sam && \
 	k8 ~/bfc/errstat.js ${DIR}/bless${SAMP}M/${SAMP}M.bless55.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.bless55.out && \
@@ -79,7 +79,7 @@ bless_trinity:
 sga:
 	mkdir -p ${DIR}/sga${SAMP}M
 	cd ${DIR}/sga${SAMP}M && \
-	sga preprocess -p 1 ${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq | gzip -1 > out.pe.fq.gz && \
+	sga preprocess -p 1 ${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq | gzip -1 > out.pe.fq.gz && \
 	sga index -a ropebwt -t 8 --no-reverse out.pe.fq.gz && \
 	sga correct -t $(CPU) -k 55 --learn out.pe.fq.gz -o /dev/stdout | tee sga.55.fq | bwa mem -p -t $(CPU) ${DIR}/genome/mus - > ${SAMP}M.sga55.sam && \
 	sga correct -t $(CPU) -k 31 --learn out.pe.fq.gz -o /dev/stdout | tee sga.31.fq | bwa mem -p -t $(CPU) ${DIR}/genome/mus - > ${SAMP}M.sga31.sam && \
@@ -97,7 +97,7 @@ sga_trinity:
 bfc:
 	mkdir -p ${DIR}/bfc${SAMP}M
 	cd ${DIR}/bfc${SAMP}M && \
-	seqtk mergepe ${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq > inter.fq && \
+	seqtk mergepe ${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq > inter.fq && \
 	bfc -s 50m -k55 -t $(CPU) inter.fq | tee bfc55.corr.fq | bwa mem -p -t $(CPU) ${DIR}/genome/mus - > ${SAMP}M.bfc55.sam && \
 	bfc -s 50m -k31 -t $(CPU) inter.fq | tee bfc31.corr.fq | bwa mem -p -t $(CPU) ${DIR}/genome/mus - > ${SAMP}M.bfc31.sam && \
 	k8 ~/bfc/errstat.js ${DIR}/bfc${SAMP}M/${SAMP}M.bfc55.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.bfc55.out && \
@@ -114,25 +114,25 @@ bfc_trinity:
 seecer:
 	mkdir -p ${DIR}/seecer${SAMP}M
 	cd ${DIR}/seecer${SAMP}M && \
-	run_seecer.sh -t . -k 31 ${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus ${DIR}/reads/subsamp_1.fastq_corrected.fa ${DIR}/reads/subsamp_2.fastq_corrected.fa > ${SAMP}M.seecer.sam && \
+	run_seecer.sh -t . -k 31 ${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq && \
+	bwa mem -t $(CPU) ${DIR}/genome/mus ${DIR}/reads/${SAMP}.subsamp_1.fastq_corrected.fa ${DIR}/reads/${SAMP}.subsamp_2.fastq_corrected.fa > ${SAMP}M.seecer.sam && \
 	k8 ~/bfc/errstat.js ${DIR}/seecer${SAMP}M/${SAMP}M.seecer.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.seecer.out
 
 seecer_trinity:
 	cd ${DIR}/seecer${SAMP}M && \
-	Trinity --seqType fa --max_memory 50G --trimmomatic --left ${DIR}/reads/subsamp_1.fastq_corrected.fa --right ${DIR}/reads/subsamp_2.fastq_corrected.fa --CPU $(CPU) --inchworm_cpu 10 --full_cleanup --quality_trimming_params "ILLUMINACLIP:${DIR}/scripts/barcodes.fa:2:40:15 LEADING:2 TRAILING:2 MINLEN:25"
+	Trinity --seqType fa --max_memory 50G --trimmomatic --left ${DIR}/reads/${SAMP}.subsamp_1.fastq_corrected.fa --right ${DIR}/reads/${SAMP}.subsamp_2.fastq_corrected.fa --CPU $(CPU) --inchworm_cpu 10 --full_cleanup --quality_trimming_params "ILLUMINACLIP:${DIR}/scripts/barcodes.fa:2:40:15 LEADING:2 TRAILING:2 MINLEN:25"
 
 rcorrector:
 	mkdir -p ${DIR}/rcorr${SAMP}M
 	cd ${DIR}/rcorr${SAMP}M && \
-	perl ~/Rcorrector/run_rcorrector.pl -t $(CPU) -k 25 -1 ${DIR}/reads/subsamp_1.fastq -2 ${DIR}/reads/subsamp_2.fastq
+	perl ~/Rcorrector/run_rcorrector.pl -t $(CPU) -k 25 -1 ${DIR}/reads/${SAMP}.subsamp_1.fastq -2 ${DIR}/reads/${SAMP}.subsamp_2.fastq
 
 rcorr_trinity:
 	cd ${DIR}/rcorr${SAMP}M && \
 	Trinity --seqType fq --max_memory 50G --trimmomatic --left ${DIR}/rcorr${SAMP}M/subsamp_1.cor.fq --right ${DIR}/rcorr${SAMP}M/subsamp_2.cor.fq --CPU $(CPU) --inchworm_cpu 10 --full_cleanup --quality_trimming_params "ILLUMINACLIP:${DIR}/scripts/barcodes.fa:2:40:15 LEADING:2 TRAILING:2 MINLEN:25"
 
 
-trinity_raw:${DIR}/reads/subsamp_1.fastq ${DIR}/reads/subsamp_2.fastq
+trinity_raw:${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq
 	mkdir -p ${DIR}/trinity_${SAMP}M
 	cd ${DIR}/trinity_${SAMP}M && \
 	Trinity --seqType fq --max_memory 50G --trimmomatic \
