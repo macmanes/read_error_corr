e5a7c57a241988d87e5bfca1a9c28cd410655a2f
Matt MacManes <matthew.macmanes@unh.edu>
2015-11-29 23:02:46 -0500

    Update correct_homo.mk

--- correct_homo.mk
+++ correct_homo.mk
@@ -42,14 +42,16 @@ scripts:
 download_reads:
 	mkdir -p ${DIR}/reads
 	cd ${DIR}/reads && \
-	curl -LO https://s3.amazonaws.com/macmanes_general/SRR797058_2.fastq.gz && \
-	curl -LO https://s3.amazonaws.com/macmanes_general/SRR797058_1.fastq.gz
+	curl -LO curl -LO ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR165/008/SRR1659968/SRR1659968_1.fastq.gz && \
+	curl -LO curl -LO ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR165/008/SRR1659968/SRR1659968_2.fastq.gz && \
+	gzip -d *gz
 
 reference:
 	mkdir -p ${DIR}/genome
 	cd ${DIR}/genome && \
-	wget ftp://ftp.ensembl.org/pub/release-79/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.chromosome.1.fa.gz && \
-	bwa index -p mus Mus_musculus.GRCm38.dna.chromosome.1.fa.gz
+	curl -LO http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.chromFa.tar.gz && \
+	tar -zxf hg38.chromFa.tar.gz && \
+	bwa index -p homo chroms/chr1.fa
 
 ${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq:
 	cd ${DIR}/reads && \
@@ -59,7 +61,7 @@ ${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq:
 raw:
 	mkdir -p ${DIR}/raw
 	cd ${DIR}/raw && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus ${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq > ${SAMP}M.raw.sam
+	bwa mem -t $(CPU) ${DIR}/genome/homo ${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq > ${SAMP}M.raw.sam
 	k8 ${BFCDIR}/errstat.js ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.raw.out && \
 	mv ${SAMP}M.raw.out ${DIR}/error_profiles/
 
@@ -68,7 +70,7 @@ lighter:${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fast
 	mkdir -p ${DIR}/lighter${SAMP}M
 	cd ${DIR}/lighter${SAMP}M && \
 	lighter -K 31 60000000 -r ${DIR}/reads/${SAMP}.subsamp_1.fastq -r ${DIR}/reads/${SAMP}.subsamp_2.fastq -t $(CPU) && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus ${SAMP}.subsamp_1.cor.fq ${SAMP}.subsamp_2.cor.fq > ${SAMP}M.lighter.sam && \
+	bwa mem -t $(CPU) ${DIR}/genome/homo ${SAMP}.subsamp_1.cor.fq ${SAMP}.subsamp_2.cor.fq > ${SAMP}M.lighter.sam && \
 	k8 ${BFCDIR}/errstat.js ${DIR}/lighter${SAMP}M/${SAMP}M.lighter.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.lighter.out && \
 	mv ${SAMP}M.lighter.out ${DIR}/error_profiles/ && \
 	rm *sam
@@ -88,8 +90,8 @@ bless:
 	cp ${KMC}/kmc/bin/kmc ${DIR}/bless${SAMP}M/kmc/bin/kmc && \
 	mpirun -np $(CPU) bless -notrim -read1 ${DIR}/reads/${SAMP}.subsamp_1.fastq -read2 ${DIR}/reads/${SAMP}.subsamp_2.fastq -prefix ${SAMP}M_bless55 -kmerlength 55 && \
 	mpirun -np $(CPU) bless -notrim -read1 ${DIR}/reads/${SAMP}.subsamp_1.fastq -read2 ${DIR}/reads/${SAMP}.subsamp_2.fastq -prefix ${SAMP}M_bless31 -kmerlength 31 && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus ${SAMP}M_bless55.1.corrected.fastq ${SAMP}M_bless55.2.corrected.fastq > ${SAMP}M.bless55.sam && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus ${SAMP}M_bless31.1.corrected.fastq ${SAMP}M_bless31.2.corrected.fastq > ${SAMP}M.bless31.sam && \
+	bwa mem -t $(CPU) ${DIR}/genome/homo ${SAMP}M_bless55.1.corrected.fastq ${SAMP}M_bless55.2.corrected.fastq > ${SAMP}M.bless55.sam && \
+	bwa mem -t $(CPU) ${DIR}/genome/homo ${SAMP}M_bless31.1.corrected.fastq ${SAMP}M_bless31.2.corrected.fastq > ${SAMP}M.bless31.sam && \
 	k8 ${BFCDIR}/errstat.js ${DIR}/bless${SAMP}M/${SAMP}M.bless55.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.bless55.out && \
 	k8 ${BFCDIR}/errstat.js ${DIR}/bless${SAMP}M/${SAMP}M.bless31.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.bless31.out && \
 	mv ${SAMP}M.bless55.out ${DIR}/error_profiles/ && \
@@ -108,40 +110,12 @@ nup --quality_trimming_params "ILLUMINACLIP:${DIR}/scripts/barcodes.fa:2:40:15 L
 	transrate -o ${SAMP}M.bless31 -a ${SAMP}M.trinity_bless31.Trinity.fasta --left ${SAMP}M_bless31.1.corrected.fastq --right ${SAMP}M_bless31.2.corrected.fastq -t $(CPU) && \
 	mv *fasta ${DIR}/assemblies/
 
-sga:
-	mkdir -p ${DIR}/sga${SAMP}M
-	cd ${DIR}/sga${SAMP}M && \
-	sga preprocess -p 1 ${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq | gzip -1 > out.pe.fq.gz && \
-	sga index -a ropebwt -t 8 --no-reverse out.pe.fq.gz && \
-	sga correct -t $(CPU) -k 55 --learn out.pe.fq.gz -o sga.55.fq && \
-	bwa mem -p -t $(CPU) ${DIR}/genome/mus sga.55.fq > ${SAMP}M.sga55.sam && \
-	sga correct -t $(CPU) -k 31 --learn out.pe.fq.gz -o sga.31.fq && \
-	bwa mem -p -t $(CPU) ${DIR}/genome/mus sga.31.fq > ${SAMP}M.sga31.sam && \
-	k8 ${BFCDIR}/errstat.js ${DIR}/sga${SAMP}M/${SAMP}M.sga55.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.sga55.out && \
-	k8 ${BFCDIR}/errstat.js ${DIR}/sga${SAMP}M/${SAMP}M.sga31.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.sga31.out && \
-	mv ${SAMP}M.sga31.out ${DIR}/error_profiles/ && \
-	mv ${SAMP}M.sga55.out ${DIR}/error_profiles/ && \
-	rm *sam
-
-sga_trinity:
-	cd ${DIR}/sga${SAMP}M && \
-	split-paired-reads.py sga.55.fq && \
-	split-paired-reads.py sga.31.fq && \
-	Trinity --seqType fq --max_memory 50G --output ${SAMP}M.trinity_sga55 --trimmomatic --left sga.55.fq.1 --right sga.55.fq.2 --CPU $(CPU) --inchworm_cpu 10 --full_cleanup --quality_trimming_params "ILLUMINACLIP:${DIR}/scripts/barcod
-es.fa:2:40:15 LEADING:2 TRAILING:2 MINLEN:25" && \
-	Trinity --seqType fq --max_memory 50G --output ${SAMP}M.trinity_sga31 --trimmomatic --left sga.31.fq.1 --right sga.31.fq.2 --CPU $(CPU) --inchworm_cpu 10 --full_cleanup --quality_trimming_params "ILLUMINACLIP:${DIR}/scripts/barcod
-es.fa:2:40:15 LEADING:2 TRAILING:2 MINLEN:25" && \
-	python3 ~/BUSCO_v1.1b1/BUSCO_v1.1b1.py -g ${SAMP}M.trinity_sga55.Trinity.fasta -m Trans --cpu $(CPU) -l ~/BUSCO_v1.1b1/vertebrata && \
-	python3 ~/BUSCO_v1.1b1/BUSCO_v1.1b1.py -g ${SAMP}M.trinity_sga31.Trinity.fasta -m Trans --cpu $(CPU) -l ~/BUSCO_v1.1b1/vertebrata && \
-	mv *fasta ${DIR}/assemblies/
-
-
 bfc ${DIR}/reads/${SAMP}.inter.fq:
 	mkdir -p ${DIR}/bfc${SAMP}M
 	cd ${DIR}/bfc${SAMP}M && \
 	seqtk mergepe ${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq > ${DIR}/reads/${SAMP}.inter.fq && \
-	bfc -s 50m -k55 -t $(CPU) ${DIR}/reads/${SAMP}.inter.fq | tee bfc55.corr.fq | bwa mem -p -t $(CPU) ${DIR}/genome/mus - > ${SAMP}M.bfc55.sam && \
-	bfc -s 50m -k31 -t $(CPU) ${DIR}/reads/${SAMP}.inter.fq | tee bfc31.corr.fq | bwa mem -p -t $(CPU) ${DIR}/genome/mus - > ${SAMP}M.bfc31.sam && \
+	bfc -s 50m -k55 -t $(CPU) ${DIR}/reads/${SAMP}.inter.fq | tee bfc55.corr.fq | bwa mem -p -t $(CPU) ${DIR}/genome/homo - > ${SAMP}M.bfc55.sam && \
+	bfc -s 50m -k31 -t $(CPU) ${DIR}/reads/${SAMP}.inter.fq | tee bfc31.corr.fq | bwa mem -p -t $(CPU) ${DIR}/genome/homo - > ${SAMP}M.bfc31.sam && \
 	k8 ${BFCDIR}/errstat.js ${DIR}/bfc${SAMP}M/${SAMP}M.bfc55.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.bfc55.out && \
 	k8 ${BFCDIR}/errstat.js ${DIR}/bfc${SAMP}M/${SAMP}M.bfc31.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.bfc31.out && \
 	mv ${SAMP}M.bfc31.out ${DIR}/error_profiles/ && \
@@ -167,7 +141,7 @@ seecer:
 	mkdir -p ${DIR}/seecer${SAMP}M
 	cd ${DIR}/seecer${SAMP}M && \
 	run_seecer.sh -t . -k 31 ${DIR}/reads/${SAMP}.subsamp_1.fastq ${DIR}/reads/${SAMP}.subsamp_2.fastq && \
-	bwa mem -t $(CPU) ${DIR}/genome/mus ${DIR}/reads/${SAMP}.subsamp_1.fastq_corrected.fa ${DIR}/reads/${SAMP}.subsamp_2.fastq_corrected.fa > ${SAMP}M.seecer.sam && \
+	bwa mem -t $(CPU) ${DIR}/genome/homo ${DIR}/reads/${SAMP}.subsamp_1.fastq_corrected.fa ${DIR}/reads/${SAMP}.subsamp_2.fastq_corrected.fa > ${SAMP}M.seecer.sam && \
 	k8 ${BFCDIR}/errstat.js ${DIR}/seecer${SAMP}M/${SAMP}M.seecer.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.seecer.out && \
 	mv ${SAMP}M.seecer.out ${DIR}/error_profiles/ && \
 	rm *sam
@@ -184,8 +158,8 @@ rm_cpu 10 --full_cleanup --quality_trimming_params "ILLUMINACLIP:${DIR}/scripts/
 rcorrector:${DIR}/reads/${SAMP}.inter.fq
 	mkdir -p ${DIR}/rcorr${SAMP}M
 	cd ${DIR}/rcorr${SAMP}M && \
-	perl ${RCORRDIR}/run_rcorrector.pl -t $(CPU) -k 31 -i ${DIR}/reads/${SAMP}.inter.fq -stdout | sed 1,8d | tee rcorr31.corr.fq| bwa mem -t $(CPU) ${DIR}/genome/mus - > ${SAMP}M.rcorr31.sam && \
-	perl ${RCORRDIR}/run_rcorrector.pl -t $(CPU) -k 55 -i ${DIR}/reads/${SAMP}.inter.fq -stdout | sed 1,8d | tee rcorr55.corr.fq | bwa mem -t $(CPU) ${DIR}/genome/mus - > ${SAMP}M.rcorr55.sam && \
+	perl ${RCORRDIR}/run_rcorrector.pl -t $(CPU) -k 31 -i ${DIR}/reads/${SAMP}.inter.fq -stdout | sed 1,8d | tee rcorr31.corr.fq| bwa mem -t $(CPU) ${DIR}/genome/homo - > ${SAMP}M.rcorr31.sam && \
+	perl ${RCORRDIR}/run_rcorrector.pl -t $(CPU) -k 55 -i ${DIR}/reads/${SAMP}.inter.fq -stdout | sed 1,8d | tee rcorr55.corr.fq | bwa mem -t $(CPU) ${DIR}/genome/homo - > ${SAMP}M.rcorr55.sam && \
 	k8 ${BFCDIR}/errstat.js ${SAMP}M.rcorr31.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.rcorr31.out && \
 	k8 ${BFCDIR}/errstat.js ${SAMP}M.rcorr55.sam ${DIR}/raw/${SAMP}M.raw.sam | tail -11 > ${SAMP}M.rcorr55.out && \
 	mv ${SAMP}M.*.out ${DIR}/error_profiles/ && \
